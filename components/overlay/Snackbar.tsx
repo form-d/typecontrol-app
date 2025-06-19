a3e3ac3ca29a8da579e01821e175056701a3e729
Heiko Owczarzak <howczarzak@Heikos-MBP-2.fritz.box>
2025-06-19 12:44:57 +0200

    snackbar improvements

--- components/overlay/Snackbar.tsx
+++ components/overlay/Snackbar.tsx
@@ -7,9 +7,10 @@ export const Snackbar: React.FC = () => {
 
   const { message, variant = "success" } = snackbarConfig;
   const baseClasses =
-    "fixed z-15 bottom-4 max-w-sm w-[calc(100%-theme(space.7))] left-1/2 transform -translate-x-1/2 px-6 py-3 text-sm md:text-base text-center rounded-lg shadow-lg";
+    "fixed z-15 bottom-4 w-auto max-w-[calc(100vw-theme(space.6))] md:max-w-sm left-1/2 transform -translate-x-1/2 px-6 py-3 text-sm md:text-base text-center rounded-lg shadow-lg";
+  // "fixed z-15 bottom-4 max-w-sm md:max-w-none w-[calc(100%-theme(space.7))] md:w-auto left-1/2 transform -translate-x-1/2 px-6 py-3 text-sm md:text-base text-center rounded-lg shadow-lg";
   const colorClasses =
-    variant === "error" ? "bg-red-600 text-white" : "bg-gray-800 text-white";
+    variant === "error" ? "bg-red-600 text-white" : "bg-gray-900 text-white";
 
   return (
     <div
